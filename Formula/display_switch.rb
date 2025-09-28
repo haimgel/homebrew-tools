869cde33bec20e6440aeb147176583d0d80e5d85
Haim Gelfenbeyn <haim@g8n.me>
2025-09-28 16:22:54 -0400

    display_switch 1.4.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/display_switch.rb
+++ Formula/display_switch.rb
@@ -4,13 +4,13 @@
 class DisplaySwitch < Formula
   desc "An utility to watch for USB device connect/disconnect events and switch display inputs via DDC/CI."
   homepage "https://github.com/haimgel/display-switch"
-  version "1.4.0"
+  version "1.4.1"
 
   depends_on "libusb"
 
   on_macos do
-    url "https://github.com/haimgel/display-switch/releases/download/1.4.0/display_switch-v1.4.0-macos-universal.zip"
-    sha256 "61911a8a70b38c710403a5c7064050f792441126ad3d0478dbf96a8308724275"
+    url "https://github.com/haimgel/display-switch/releases/download/1.4.1/display_switch-v1.4.1-macos-universal.zip"
+    sha256 "d1ea418148aeadf2ec350787fd04c8a56a765610454e6db4abba143956edadb5"
 
     def install
       bin.install "display_switch"
